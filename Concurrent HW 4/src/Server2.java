617dd299a2b21f9c547dea00f4f12c0db56f289e
rahjah1 <roger_2009@ymail.com>
2015-03-25 16:48:36 -0500

    Tried to work on UpdateAll

--- Concurrent HW 4/src/Server2.java
+++ Concurrent HW 4/src/Server2.java
@@ -18,35 +18,68 @@ public class Server2 {
 
 	private static int MAX_BOOKS;	//Books included with input
 	private static int TCP_PORT;	//TCP port included with instructions
+	private static int serverID;
 
 	public static class ServerThread extends Thread {
 
 		private ConcurrentHashMap<Integer, Integer> store;
 		private ArrayList<String> serverProx;
-		private ConcurrentLinkedQueue<Integer> requests;
+		private static int requests[];
 		private ConcurrentLinkedQueue<String> crashQ;
 		private Socket tcp_socket = null;
 		private int commandsServed = 0; 
+		private static DirectClock clock;
 		String crashCommand;
 
-		public ServerThread(ConcurrentHashMap<Integer, Integer> inBS, Socket inS, ConcurrentLinkedQueue<String> crash, ArrayList<String> servers, ConcurrentLinkedQueue<Integer> req) 
+		public ServerThread(ConcurrentHashMap<Integer, Integer> inBS, Socket inS,
+				ConcurrentLinkedQueue<String> crash, ArrayList<String> servers, int[] req, DirectClock v) 
 		{
 			requests = req;
 			serverProx = servers;
 			crashQ = crash;
 			store = inBS;
 			tcp_socket = inS;
+			clock=v;
 		}
 
 		public synchronized static void UpdateAll(String change)
-		{	
-			//Method for updating all the other servers
+		{	//requestCS
+			clock.tick();									//Increment clock
+			requests[serverID] = clock.getValue(serverID);	//Add own timestamp to the request queue
+			//broadcastMsg("request", requests[serverID]);	//Send request message, activating the other server threads' listeners
+			while(!okayCS()){								//If update is not the earliest
+				//checkforMessage();						//Check for message
+			}
+			//CS
+			//broadcastMsg("change", change)
+			
+			//releaseCS
+			requests[serverID] = -1;
+			//broadcastMsg("release", clock.getValue(serverID));
 			//Lamport mutex goes here: logical clock, queue for update requests, handling acks, etc.
 			//THe server will push the state of its bookstore to other servers
 			//Server will communicate changes by forwarding the client's command to the other servers
 			return;
 		}
-		
+		public static boolean okayCS(){
+			for(int j=0; j<serverID;j++){
+				if(isGreater(requests[serverID],serverID,requests[j],j))
+					return false;
+				if(isGreater(requests[serverID],serverID, clock.getValue(j),j))
+					return false;
+				
+			}
+			return true;
+		}
+		public static boolean isGreater(int entry1, int pid1, int entry2, int pid2)
+		{
+			if(entry2==-1) return false;
+			return((entry1>entry2)||((entry1==entry2)&& (pid1>pid2)));
+		}
+		//public synchronized void handleMsg
+		//{
+		//	
+		//}
 		public static void Recover() //What a crash thread executes to sync it's own data back. Synchronized?
 		{	
 			//If a library is currently updating other servers, wait(); 	
@@ -70,7 +103,7 @@ public class Server2 {
 					int timeout = Integer.parseInt(crashCa[2]);
 					if(commandsServed >= crashAfter) // Assuming that 
 					{
-						crashQ.remove(0);
+						crashQ.poll();
 						store.clear();
 						Thread.sleep((long)timeout);
 					 	Recover();
@@ -82,11 +115,15 @@ public class Server2 {
 					int client_num = in.nextInt();					//Take client command
 					int book_num = in.nextInt();					
 					String cmd_returned = in.next();				
-					if(cmd_returned.contains("server"))
+					if(cmd_returned.contains("request"))			//
 					{
 						
 					}
-					if (cmd_returned.equals("reserve")) 
+					else if(cmd_returned.contains("server"))
+					{
+						
+					}
+					else if (cmd_returned.equals("reserve")) 
 					{
 						// reserve a book_num
 						if (book_num >= 1 && book_num <= MAX_BOOKS && store.putIfAbsent(book_num, client_num) == null) 
@@ -143,21 +180,23 @@ public class Server2 {
 	public static void main(String[] args) {
 		final ConcurrentHashMap<Integer, Integer> book_store = new ConcurrentHashMap<Integer, Integer>();
 		final ArrayList<String> serverProx = new ArrayList<String>(); //List of other servers
-		ConcurrentLinkedQueue<Integer> requests = new ConcurrentLinkedQueue<Integer>();
 		ConcurrentLinkedQueue<String> crashQ = new ConcurrentLinkedQueue<String>(); //List of crash commands (if there are more than 1 TODO: determine whether the server only looks at one crash at a time
 		Scanner in = new Scanner(System.in);	//Setup server input
-		int serverID = in.nextInt();			//Get Server ID
+		serverID = in.nextInt();			//Get Server ID
 		int totalServers = in.nextInt();		//Get number of servers
 		MAX_BOOKS = in.nextInt();				//Get number of books
+		int []requests = new int[totalServers];
 		for(int i = 0; i < totalServers; i++)
 		{
 			serverProx.add(in.nextLine());
+			requests[i]=-1;		//Request timestamps set to -1
 		}
 		while(in.hasNext()) //Get crash instructions, may have multiple lines
 		{		
 			crashQ.add(in.nextLine());//put crash instruction into queue, access the instruction later
 		}
 		in.close();
+		DirectClock v = new DirectClock(totalServers,serverID);
 		TCP_PORT = Integer.parseInt(serverProx.get(serverID - 1).substring(serverProx.get(serverID - 1).indexOf(":")+1));
 		Thread tcpThread = new Thread(new Runnable() //Maybe put all Threads in ThreadPool to start all later?
 		{ 
@@ -170,7 +209,7 @@ public class Server2 {
 					while ((sock = collector.accept()) != null) 
 					{
 						System.out.println("New TCP connection from " + sock.getInetAddress());
-						Thread t = new ServerThread(book_store, sock, crashQ, serverProx, requests);
+						Thread t = new ServerThread(book_store, sock, crashQ, serverProx, requests,v);
 						t.start();
 					}
 					collector.close();
