45ad1dbda79f4edfe83009234d41f60992f052a2
Josh Havens <thejrh@ymail.com>
2015-03-24 20:53:01 -0500

    Fixed a lot of stuff, need to implement Recover and Update

--- Concurrent HW 4/src/Server2.java
+++ Concurrent HW 4/src/Server2.java
@@ -10,15 +10,18 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server2 {
 
 	private static int MAX_BOOKS;	//Books included with input
 	private static int TCP_PORT;	//TCP port included with instructions
-	static ArrayList<String> crashQ = new ArrayList<String>();
+
 	public static class ServerThread extends Thread {
 
 		private ConcurrentHashMap<Integer, Integer> store;
+		private ArrayList<String> crashQ = new ArrayList<String>();
 		private Socket tcp_socket = null;
 		private int commandsServed = 0; 
 		String crashCommand;
@@ -106,43 +109,45 @@ public class Server2 {
 	public static void main(String[] args) {
 		final ConcurrentHashMap<Integer, Integer> book_store;
 		book_store = new ConcurrentHashMap<Integer, Integer>();
-		ArrayList<String> serverProx = new ArrayList<String>();
+		ArrayList<String> serverProx = new ArrayList<String>(); //List of other servers
+		ArrayList<String> crashQ = new ArrayList<String>(); //List of crash commands (if there are more than 1)
 		Scanner in = new Scanner(System.in);	//Setup server input
 		int serverID = in.nextInt();			//Get Server ID
 		int totalServers = in.nextInt();		//Get number of servers
 		MAX_BOOKS = in.nextInt();				//Get number of books
-		int setupServers = 0; 
-		////InetAddress address;
-		while(setupServers < totalServers)
+		for(int i = 0; i < totalServers; i++)
 		{
 			serverProx.add(in.nextLine());
-			//address = InetAddress.getByName(temp.substring(0,temp.indexOf(":")).trim());
-		
-			TCP_PORT = in.nextInt();//TCP_PORT = Integer.parseInt(temp.substring(temp.indexOf(":")+1));
-													//in.close(); Moved to bottom
-			Thread tcpThread = new Thread(new Runnable() { //Maybe put all Threads in ThreadPool to start all later?
-				public void run() {						   // Still need to have them in order, maybe queue for connecting later
-					try {
-						ServerSocket collector = new ServerSocket(TCP_PORT);
-						Socket sock;
-						while ((sock = collector.accept()) != null) {
-							System.out.println("New TCP connection from " + sock.getInetAddress());
-							Thread t = new ServerThread(book_store, sock);
-							t.start();
-						}
-						collector.close();
-					} catch (Exception e) { }
-				}
-			});
-			setupServers++;	//Increment number of servers setup
-			//TODO:Add to threadpool
 		}
 		while(in.hasNext()) //Get crash instructions, may have multiple lines
 		{		
 			crashQ.add(in.nextLine());//put crash instruction into queue, access the instruction later
 		}
-		//TODO:Executor service call to run threadpool
 		in.close();
+		TCP_PORT = Integer.parseInt(serverProx.get(serverID - 1).substring(serverProx.get(serverID - 1).indexOf(":")+1));
+		Thread tcpThread = new Thread(new Runnable() //Maybe put all Threads in ThreadPool to start all later?
+		{ 
+			public void run() // Still need to have them in order, maybe queue for connecting later
+			{						   
+				try
+				{
+					ServerSocket collector = new ServerSocket(TCP_PORT);
+					Socket sock;
+					while ((sock = collector.accept()) != null) 
+					{
+						System.out.println("New TCP connection from " + sock.getInetAddress());
+						Thread t = new ServerThread(book_store, sock);
+						t.start();
+					}
+					collector.close();
+				} 
+				catch (Exception e) { }
+			}
+		});
+		
+		//Run all of the servers
+		tcpThread.start();
+		
 	}
 	//TODO:
 	public static void Recover() //What a crash thread executes to sync it's own data back. Synchronized?
@@ -160,6 +165,8 @@ public class Server2 {
 	{	
 		//Method for updating all the other servers
 		//Lamport mutex goes here: logical clock, queue for update requests, handling acks, etc.
+		//THe server will push the state of its bookstore to other servers
+		//Server will communicate changes by forwarding the client's command to the other servers
 		return;
 	}
 
